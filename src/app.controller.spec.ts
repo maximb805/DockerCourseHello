fe4c5410109a2dd6d4ccc3c99d1ac2dc21e7e490
Maxim <maximb805@gmail.com>
2024-08-26 22:00:39 +0300

    one ! added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Goodbye World!"', () => {
-      expect(appController.getHello()).toBe('Goodbye World!');
+      expect(appController.getHello()).toBe('Goodbye World!!');
     });
   });
 });
